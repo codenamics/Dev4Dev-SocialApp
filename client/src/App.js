66bae0bb470c45c515b2ff16ce7d09cfdfba6dd3
Codenamics <info@codenamics.pl>
2018-06-15 01:04:00 +0200

    api call/axios/issue with proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import { hot } from "react-hot-loader";
 import * as React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { hot } from "react-hot-loader";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import "./styles/theme.scss"
 
@@ -10,11 +13,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <NavBar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+
+        <div>
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+
+      </Router>
 
     )
 
